da3dfb235693059a4c5e8fde6847386457cbd24a
DamienCM <damien.cartiermillon@gmail.com>
2021-03-22 20:14:41 +0100

    adding initial files

---
+++ documentation/approvisionnement/pre_dimmensionnement.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Pre dimmensionnement 
+# - On determine la vitesse des disques de freins 
+# - On determine les differents types d'actionneurs 
+
+#Extrait du sujet :
+# ➢ Actionneur permettant l’arrêt et la distribution des disques un par un
+# ➢ guidage des disques dans la zone de préhension (suivant besoin fonctionnel)
+# ➢ système de préhension➢définition de la structure et intégration
+
